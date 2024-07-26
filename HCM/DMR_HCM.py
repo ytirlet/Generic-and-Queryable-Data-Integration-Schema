0f8575df7218134bb9fbfd90653bc57567f8fc79
ytirlet <101868688+ytirlet@users.noreply.github.com>
2024-07-26 09:31:43 +0200

    Update DMR_HCM.py

--- HCM/DMR_HCM.py
+++ HCM/DMR_HCM.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 # Opening and reading result files #################################
-xls4 = pd.ExcelFile("/home/ytirlet/Documents/HCM/Supplemental_tables/Table4.xlsx")
+xls4 = pd.ExcelFile(PATH_TO_DMR_FILE)
 dico_sheet4 = pd.read_excel(xls4,sheet_name=None)
 names4 = xls4.sheet_names
 DMRhyper = dico_sheet4[names4[0]]
@@ -23,4 +23,4 @@ DMRs = pd.concat([DMRhyper,DMRhypo],axis=0)
 DMRs.insert(0,'Differential Methylated Regions',['DiffMeth_' + str(i) for i in range(len(DMRs))])
 DMRs.insert(5,'measured_in@Contrast',["HCMvsC" for i in range(len(DMRs))])
 
-DMRs.to_csv("/home/ytirlet/Documents/HCM/Supplemental_tables/integr/DMRs.tsv",sep='\t',index=False)
+DMRs.to_csv(NEW_DMR_FILE,sep='\t',index=False)
