8b4db1358454037c77f18089d1ecaeb7848e4a7c
ytirlet <101868688+ytirlet@users.noreply.github.com>
2024-07-26 09:58:58 +0200

    Update DEGs_HBee.py

--- HoneyBee/DEGs_HBee.py
+++ HoneyBee/DEGs_HBee.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 # Opening and reading result files #################################
-xls1 = pd.ExcelFile("/home/ytirlet/Documents/honeybee/Table7.xlsx")
+xls1 = pd.ExcelFile(PATH_TO_DEG_FILE)
 dico_sheet1 = pd.read_excel(xls1,sheet_name=None)
 
 names1 = xls1.sheet_names
@@ -39,4 +39,4 @@ DEGs = pd.concat([Deg_UP2,Deg_DOWN2,Deg_UP4,Deg_DOWN4],axis=0)
 DEGs.insert(0,'Differential Expression',['DEG_' + str(i) for i in range(len(DEGs))])
 DEGs = DEGs.rename(columns={'gene_name':'measured_in@gene'})
 
-DEGs.to_csv("/home/ytirlet/Documents/honeybee/integr/DEGs.tsv",sep='\t',index=False)
+DEGs.to_csv(NEW_DEG_FILE,sep='\t',index=False)
